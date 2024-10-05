0f09de93914f9ea2c817aeb565f7ebc09f55c935
Meredick <meredick.rg@gmail.com>
2024-10-04 22:55:46 -0400

    Design
    Here we've worked a little bit of design and made some corrections.

--- Biblioteca/Controllers/LibrosController.cs
+++ Biblioteca/Controllers/LibrosController.cs
@@ -20,6 +20,127 @@ namespace BibliotecaMVC.Controllers
             return View(await _context.Libros.ToListAsync());
         }
 
-        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Titulo,Autor,AnioPublicacion,Genero,Disponible")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(libro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libro);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.Libros.FindAsync(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            return View(libro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titulo,Autor,AnioPublicacion,Genero,Disponible")] Libro libro)
+        {
+            if (id != libro.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(libro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LibroExists(libro.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw; 
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libro);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.Libros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+       
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libro = await _context.Libros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var libro = await _context.Libros.FindAsync(id); 
+
+            
+            if (libro == null)
+            {
+                return NotFound(); 
+            }
+
+          
+            _context.Libros.Remove(libro); 
+            await _context.SaveChangesAsync(); 
+            return RedirectToAction(nameof(Index)); 
+        }
+
+        private bool LibroExists(int id)
+        {
+            return _context.Libros.Any(e => e.Id == id);
+        }
     }
 }
